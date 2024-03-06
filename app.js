4f2465d15f849fc4a2d3b91b228ecd3642a0799f
MangalaParameswaran <mangala214128pa@gmail.com>
2024-03-06 13:24:16 +0530

    code updated in app.js

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    // app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        // res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        res.send('Back-end Application Is Running Successfully')
     })
 }
 
